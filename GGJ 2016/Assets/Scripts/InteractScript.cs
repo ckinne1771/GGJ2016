1317f674cc52f2e7338409ba6e13caa1605a7acf
Baranzo94 <Liam@Liams-MacBook-Pro.local>
2016-01-29 19:57:02 +0000

    Fri 20:00
    -FPS Controller
    -Plane
    -Changed Unity Inputs
    -Added script

---
+++ GGJ 2016/Assets/Scripts/InteractScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InteractScript : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject interactObject;
+
+	public Text dialogue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onTriggerEnter()
+	{
+		
+	}
+
+	void onTriggerStay()
+	{
+		
+	}
+
+	void onTriggerExit()
+	{
+		
+	}
+
+}
