5e966ef38e0d1685fb28d722ff38a268090513b1
Baranzo94 <Liam@Liams-MacBook-Pro.local>
2016-01-30 20:19:37 +0000

    Additional Scenes and art stuff

---
+++ GGJ 2016/Assets/Scripts/BackScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void changeScene( int sceneToChangeTo )
+	{
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
